b3157eccf55b25a513a0d725cdc2b47afd85d0c7
LivingWill <william.hinson@outlook.com>
2021-02-03 21:53:03 -0600

    background fixed styling-now adjusts fullscreen

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import './style.css';
 import Todo from './creation';
 import { landing } from './landingModule.js';
-import { setPic } from './pic.js'
-
-
+// import { setPic } from './pic.js'
 
 const content = document.querySelector('#content');
-content.appendChild(setPic())
+
 content.appendChild(landing())
--- src/pic.js
+++ src/pic.js
@@ -1,8 +1,8 @@
-import Picture from './pencil.jpg'
-const setPic = () => {
-    const pencilPic = new Image();
-    pencilPic.src = Picture;
-    pencilPic.setAttribute('id', 'pencilBG')
-    return pencilPic;
-}
-export { setPic }
+// import Picture from './pencil.jpg'
+// const setPic = () => {
+//     const pencilPic = new Image();
+//     pencilPic.src = Picture;
+//     pencilPic.setAttribute('id', 'pencilBG')
+//     return pencilPic;
+// }
+// export { setPic }
