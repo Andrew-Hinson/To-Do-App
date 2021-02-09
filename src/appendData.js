4f6c299e25a966e990ccb3db0f21f7924f7c4010
LivingWill <william.hinson@outlook.com>
2021-02-09 17:54:20 -0600

    styling todos

--- src/appendData.js
+++ src/appendData.js
@@ -4,15 +4,26 @@ const appendToFront = () => {
     const flipCardFront = document.querySelector('.flip-card-front')
     const lastIndex = taskData.tasks.length - 1;
     const newTask = document.createElement('div')
-    const taskTitle = document.createElement('h3')
-    const checkBox = document.createElement('checkbox')
+    const taskTitle = document.createElement('p')
+    const checkLabel = document.createElement('label')
+    const checkBox = document.createElement('input')
     
     let taskObject = taskData.tasks[lastIndex]
 
+    checkBox.setAttribute('type', 'checkbox')
+    checkBox.setAttribute('id', 'checkbox')
+    checkLabel.setAttribute('for', 'checkbox')
     newTask.classList.add('todo')
+
+
     taskTitle.innerText = `${taskObject.title}`
+
+    checkLabel.innerText = 'Complete'
+
     newTask.appendChild(taskTitle)
-    newTask.appendChild(checkBox)
+    checkLabel.appendChild(checkBox)
+    newTask.appendChild(checkLabel)
+   
     
     flipCardFront.appendChild(newTask)
     return flipCardFront
