f08c0bb6dbba757c50963292ed9d2488940ae560
LivingWill <william.hinson@outlook.com>
2021-02-27 18:55:27 -0600

    implimenting class detection when adding item

--- src/index.js
+++ src/index.js
@@ -38,61 +38,72 @@ const notesInput = document.querySelector('#notesInput')
 
 
 
-// when button is clicked for accept, create new task and push to class Display
-const workDisplay = new Display(cards)
-const studyDisplay = new Display(cards)
-const personalDisplay = new Display(cards)
-const choresDisplay = new Display(cards)
+//value of the selected category to put the task inside
+let categoryDisplay; 
 
 
- const dialogControl = (() => {
-      let target;
-      
-      const dialog = document.querySelector('#dialog');
-      let h2Span = document.querySelector('#dialogH2Span')
-      let catTitle = document.querySelector('#categoryTitle')
-         // Form cancel button closes the dialog box
-      cancel.addEventListener('click', (e) => {
-            e.preventDefault()
-            dialog.close();
-      });
-      //clicking menu option opens dialog
-      const menuOptions = document.querySelectorAll('.newFlip')
-            for(const option of menuOptions){
-                  
-                  option.addEventListener('click', (e) => {
-                        target = e.target
-                        h2Span.innerText = `${target.id}`
-                        h2Span.setAttribute('value', `${target.value}`)
-                        catTitle.innerText = `${target.id}`
+const dialogControl = (() => {
+     let target;
+     
+     const dialog = document.querySelector('#dialog');
+     let h2Span = document.querySelector('#dialogH2Span')
+     let catTitle = document.querySelector('#categoryTitle')
+        // Form cancel button closes the dialog box
+     cancel.addEventListener('click', (e) => {
+           e.preventDefault()
+           dialog.close();
+     });
+     //clicking menu option opens dialog
+     const menuOptions = document.querySelectorAll('.newFlip')
+           for(const option of menuOptions){
+                 
+                 option.addEventListener('click', (e) => {
+                       target = e.target
+                       categoryDisplay = `${target.value}` //maincontrol of which category we are in
+                       h2Span.innerText = `${target.id}`
+                       h2Span.setAttribute('value', `${target.value}`)
+                       catTitle.innerText = `${target.id}`
                        
-                        dialog.show()
-                  });
-            };
- })();
+                       dialog.show()
+                 });
+           };
+})();
  
 
+const workDisplay = new Display(cards)
+const choresDisplay = new Display(cards)
+const studyDisplay = new Display(cards)
+const personalDisplay = new Display(cards)
 
+
+// when button is clicked for accept, create new task and push to class Display
 //when accept is pressed, it should place the card in the selected category
-      accept.addEventListener('click', (e) => {
-            e.preventDefault()
-            //radio selection to determine priority
-            const radioInputs = document.getElementsByTagName('input')
-            let catValue = document.querySelector('#dialogH2Span')
-            let radioValue; //assigned a number 1-3 green - red, loops over title input as well but not a factor
-            for(let radio of radioInputs){
-                  if(radio.checked === true){
-                        radioValue = radio.value
-                  }
+accept.addEventListener('click', (e) => {
+      e.preventDefault()
+      //radio selection to determine priority
+      const radioInputs = document.getElementsByTagName('input')
+            //can use this to figure out which Display Class to use
+      let radioValue; //assigned a number 1-3 green - red, loops over title input as well but not a factor
+      for(let radio of radioInputs){
+            if(radio.checked === true){
+                  radioValue = radio.value
             }
-            const task = new Task(titleInput.value, notesInput.value, radioValue)
+      }
+      const task = new Task(titleInput.value, notesInput.value, radioValue)
+      
+      if(categoryDisplay === '0'){
             workDisplay.add(task)
+      } else if(categoryDisplay === '1'){
+            choresDisplay.add(task)
+      } else if(categoryDisplay === '2'){
+            personalDisplay.add(task)
+      } else if(categoryDisplay === '3'){
+            studyDisplay.add(task)
+      }
 
-            titleInput.value = '';
-            notesInput.value = '';
-
-            dialog.close()
-
+      titleInput.value = '';
+      notesInput.value = '';
+      dialog.close()
 });
 
 
